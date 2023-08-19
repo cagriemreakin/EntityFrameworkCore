69bb8ebef9c388cac642491410564160cde0a723
cagriemreakin <cagriemreakin@hotmail.com>
2023-08-19 22:14:00 +0300

    Table per table

--- DAL/AppDbContext.cs
+++ DAL/AppDbContext.cs
@@ -5,51 +5,26 @@ namespace CodeFirst.DAL
 {
 	public class AppDbContext:DbContext
 	{
-		public DbSet<Product> Products{ get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<ProductFeature> ProductFeatures { get; set; }
-        public DbSet<Teacher> Teachers { get; set; }
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Manager> Managers { get; set; }
+
+
+        public DbSet<BasePerson> Persons { get; set; }
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             Initializer.Build();
-            optionsBuilder
-                          .UseSqlServer(Initializer.Configuration.GetConnectionString("SqlCon"));
+            optionsBuilder.UseSqlServer(Initializer.Configuration.GetConnectionString("SqlCon"));
                
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Product>().Property(x => x.Price).HasPrecision(18,2);
-
-            modelBuilder.Entity<Product>().Property(x => x.PriceKdv).HasComputedColumnSql("[Price] * [Kdv]");
-            //modelBuilder.Entity<Product>().Property(x => x.PriceKdv).ValueGeneratedNever(); -> None
-            //modelBuilder.Entity<Product>().Property(x => x.PriceKdv).ValueGeneratedOnAdd(); -> Identity
-            //modelBuilder.Entity<Product>().Property(x => x.PriceKdv).ValueGeneratedOnAddOrUpdate(); -> Computed
-
-
-            modelBuilder.Entity<Product>().HasOne(x => x.ProductFeature).WithOne(x => x.Product).HasForeignKey<ProductFeature>(x => x.Id);
-
-            // we can change delete behaviour of our table in here Cascade,Restrict,SetNull or NoAction
-            modelBuilder.Entity<Category>().HasMany(x => x.Products)
-                                           .WithOne(x => x.Category)
-                                           .HasForeignKey(x => x.CategoryId)
-                                           .OnDelete(DeleteBehavior.Restrict);
-
-            // EF Core version >= 5 creates cross table for the tables which have many to many relationship with each other.
-
-            // if we want to create cross table using ef core convention, we need to define it on model creating
-
-            
-            modelBuilder.Entity<Student>().HasMany(x => x.Teachers)
-                                          .WithMany(x => x.Students)
-                                          .UsingEntity<Dictionary<string, object>>(
-                                             "StudentTeacherCrossTable",
-                                             x => x.HasOne<Teacher>().WithMany().HasForeignKey("Teacher_Id").HasConstraintName("FK__TeacherId"),
-                                             x=> x.HasOne<Student>().WithMany().HasForeignKey("Student_Id").HasConstraintName("FK__StudentId")) ;
+            //TPT
+            modelBuilder.Entity<BasePerson>().ToTable("Persons");
+            modelBuilder.Entity<Employee>().ToTable("Employees");
+            modelBuilder.Entity<Manager>().ToTable("Managers");
 
             base.OnModelCreating(modelBuilder);
         }
--- Program.cs
+++ Program.cs
@@ -7,12 +7,9 @@ Initializer.Build();
 
 using (var _context = new AppDbContext())
 {
-    //lazy loading
-    var category = _context.Categories.First();
-    //lazy loading stars here. It makes second query to access products of category;
-    var products = category.Products;
-
-
+    _context.Employees.Add(new Employee { FirstName = "Emre", LastName = "Akın", Age = 30, Salary = 10 });
+    _context.Managers.Add(new Manager { FirstName = "Ali", LastName = "Akın", Age = 30, Grade=5 });
+    _context.SaveChanges();
 }
 
 
