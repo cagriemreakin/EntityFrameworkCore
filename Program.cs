70861b2b11bd510d157720e7c47d1904592369cd
cagriemreakin <cagriemreakin@hotmail.com>
2024-07-08 23:02:18 +0300

    raw sql

--- Program.cs
+++ Program.cs
@@ -9,29 +9,11 @@ using (var _context = new AppDbContext())
 {
     try
     {
-        //join
-        var result = (from c in _context.Categories
-                      join p in _context.Products on c.Id equals p.CategoryId
-                      select new {p}
-                      ).ToListAsync();
+        int id = 5;
+        var rawSql = await _context.Products.FromSqlRaw("select * from Products where id={0}", id).ToListAsync();
 
-        //left join
-        var leftJoin = await (from c in _context.Categories
-                      join pl in _context.Products on c.Id equals pl.CategoryId into plist
+        var interpolatedSql = await _context.Products.FromSqlInterpolated($"select * from Products where id={id}").ToListAsync();
 
-                      from pl in plist.DefaultIfEmpty()
-                      select new {c, pl }
-                ).ToListAsync();
-        //right join
-        var rightJoin = await (from pl in _context.Products
-                              join c in _context.Categories on pl.CategoryId equals c.Id into plist
-
-                              from c in plist.DefaultIfEmpty()
-                              select new { c, pl }
-                ).ToListAsync();
-
-        //full outer join
-        var fullOurterJoin = leftJoin.Union(rightJoin);
 
 
     }
